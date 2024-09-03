5e469b18c7315de9b7d121e62751b4fc2161dab6
Cristian Manuel Orozco <orozcocristian860@gmail.com>
2024-09-03 15:14:29 -0300

    add data base init

---
+++ composeApp/src/androidMain/kotlin/com/wolfconbridge/gastos/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.wolfconbridge.gastos
+
+/**
+ * @author Cristian Manuel Orozco - Orozcocristian860@gmail.com
+ * @created 23/08/2024 20:21
+*/class MainApplication {
+}
---
+++ composeApp/src/commonMain/kotlin/di/appModule.kt
@@ -0,0 +1,6 @@
+package di
+
+/**
+ * @author Cristian Manuel Orozco - Orozcocristian860@gmail.com
+ * @created 23/08/2024 20:28
+*/
